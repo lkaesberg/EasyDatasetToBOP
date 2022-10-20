15cc5ea558287f57cb6019154f9bd6cc1681d797
lars <larskaesberg@yahoo.de>
2022-10-20 04:01:01 +0200

    fix: setup.py

--- easydatasettobop/__main__.py
+++ easydatasettobop/__main__.py
@@ -1,14 +1,14 @@
 import argparse
-
-from easydatasettobop.dataset_utils.visualization_dataset import visualize_dataset
 from scripts.convert_easy_dataset_to_bop import convert_dataset
+from dataset_utils.visualization_dataset import visualize_dataset
 
 
 def get_arguments():
     parser = argparse.ArgumentParser("Convert EasyDataset to BOP Dataset")
 
     parser.add_argument('path', help="Path to the root of the EasyDatasetGenerator", type=str)
-    parser.add_argument('-v', '--visualize', help="Visualize a converted dataset. Path of the train set (i.e. */bopdataset/train/000000)",
+    parser.add_argument('-v', '--visualize',
+                        help="Visualize a converted dataset. Path of the train set (i.e. */bopdataset/train/000000)",
                         action="store_true")
     parser.add_argument('-e', '--ending', help="Generate the .ply models from specified file format", type=str,
                         default=".gltf")
--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 import setuptools
 
+import easydatasettobop
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="EasyDataSetToBOP",  # This is the name of the package
-    version="1.0.0",  # The initial release version
+    version="1.0.1",  # The initial release version
     author="Lars Kaesberg",  # Full name of the author
     description="Convert Dataset from EasyDatasetGenerator to BOP Dataset",
     long_description=long_description,  # Long description read from the the readme file
     long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),  # List of all python modules to be installed
+    packages=["easydatasettobop", "easydatasettobop.scripts", "easydatasettobop.dataset_utils",
+              "easydatasettobop.bop_dataset_utils"],  # List of all python modules to be installed
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
@@ -19,5 +22,6 @@ setuptools.setup(
     python_requires='>=3.9',  # Minimum version requirement of the package
     py_modules=["easydatasettobop"],  # Name of the python package
     package_dir={'': '.'},  # Directory of the source code of the package
-    install_requires=["pymeshlab", "imageio", "pypng", "opencv-python", "tqdm", "scipy", "numpy"]  # Install other dependencies if any
+    install_requires=["pymeshlab", "imageio", "pypng", "opencv-python", "tqdm", "scipy", "numpy"]
+    # Install other dependencies if any
 )
